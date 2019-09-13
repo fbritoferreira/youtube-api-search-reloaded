453223a07f9c33e1b52beaa5b8da7adcd64d3e58
iTsFILIPOficial <filipe.ferreira@sky.uk>
2019-09-13 15:21:38 +0100

    Added uglify and minify to bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,32 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import json from 'rollup-plugin-json';
+import { terser } from 'rollup-plugin-terser';
 
 const dist = 'dist';
+const globals = {
+  axios: 'axios',
+};
 
 export default {
   input: 'src/index.js',
+  external: ['axios'],
   output: [
     {
       file: `${dist}/bundle.cjs.js`,
       format: 'cjs',
+      globals,
     },
     {
       file: `${dist}/bundle.esm.js`,
       format: 'esm',
+      globals,
     },
     {
       name: 'youtubeApiSearchReloaded',
       file: `${dist}/bundle.umd.js`,
       format: 'umd',
+      globals,
     },
   ],
   plugins: [
@@ -29,5 +37,8 @@ export default {
       include: 'node_modules/**',
     }),
     json(),
+    terser({
+      compress: true,
+    }),
   ],
 };
