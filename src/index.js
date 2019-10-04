e9145ed0094928307363148a0f25ff9139684b6d
Filipe Ferreira <filipe.ferreira@sky.uk>
2019-10-04 11:20:20 +0100

    Removed axios

--- src/index.js
+++ src/index.js
@@ -1,77 +1,18 @@
-import axios from 'axios';
+import fetch from 'isomorphic-unfetch';
+import { URL, URLSearchParams } from 'url';
 
 export const ROOT_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-export const youtubeSearch = ({
-  key,
-  part,
-  term,
-  type,
-  forContentOwner,
-  forDeveloper,
-  forMine,
-  relatedToVideoId,
-  channelId,
-  channelType,
-  eventType,
-  location,
-  maxResults,
-  onBehalfOfContentOwner,
-  order,
-  pageToken,
-  publishedAfter,
-  regionCode,
-  relevanceLanguage,
-  safeSearch,
-  topicId,
-  videoCaption,
-  videoCategoryId,
-  videoDefinition,
-  videoDimension,
-  videoDuration,
-  videoEmbeddable,
-  videoLicense,
-  videoSyndicated,
-  videoType,
-}) => new Promise((resolve, reject) => {
-  if (!key || !term || !part || !type) {
+export const youtubeSearch = (options = {}) => new Promise((resolve, reject) => {
+  if (!options.key || !options.term || !options.part || !options.type) {
     reject(new Error('Please make sure that the required fields are inserted'));
   }
 
-  axios
-    .get(ROOT_URL, {
-      key,
-      part,
-      term,
-      type,
-      forContentOwner,
-      forDeveloper,
-      forMine,
-      relatedToVideoId,
-      channelId,
-      channelType,
-      eventType,
-      location,
-      maxResults,
-      onBehalfOfContentOwner,
-      order,
-      pageToken,
-      publishedAfter,
-      regionCode,
-      relevanceLanguage,
-      safeSearch,
-      topicId,
-      videoCaption,
-      videoCategoryId,
-      videoDefinition,
-      videoDimension,
-      videoDuration,
-      videoEmbeddable,
-      videoLicense,
-      videoSyndicated,
-      videoType,
-    })
-    .then((response) => resolve(response.data.items))
+  const url = new URL(ROOT_URL);
+  url.search = new URLSearchParams(options);
+
+  fetch(url.href)
+    .then((response) => resolve(response.json()))
     .catch(reject);
 });
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,13 @@
-import axios from 'axios';
-import MockAxios from 'axios-mock-adapter';
-import { youtubeSearch, ROOT_URL } from './index';
+import dotenv from 'dotenv';
+import { youtubeSearch } from './index';
+
+dotenv.config();
+
+const { API_KEY } = process.env;
 
 describe('YoutubeSearch', () => {
-  beforeEach(() => {
-    const mock = new MockAxios(axios);
-    mock.onGet(ROOT_URL).reply(200, {
-      items: ['item1', 'item2'],
-      test: 'passed',
-    });
-  });
-
   it('Returns error if required parameters are not passed in', () => youtubeSearch({
-    key: 'API_KEY',
+    key: API_KEY,
     term: 'John Cena',
     part: 'snippet',
   }).catch((error) => {
@@ -20,11 +15,11 @@ describe('YoutubeSearch', () => {
   }));
 
   it('Return correct data when arguments are correct', () => youtubeSearch({
-    key: 'API_KEY',
+    key: API_KEY,
     term: 'John Cena',
     part: 'snippet',
     type: 'video',
   })
-    .then((data) => expect(data).toEqual(['item1', 'item2']))
+    .then((data) => expect(data).toEqual(expect.anything()))
     .catch((error) => expect(error).toBeNull()));
 });
